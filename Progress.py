f1f60716d047830077794a15fa7f28d749d5a129
Ed Morris <emorris@esriuk.com>
2018-08-01 12:37:38 +0100

    Add files via upload

---
+++ Progress.py
@@ -0,0 +1,17 @@
+## Ed's Demo script for PythonWin
+
+myList = ["Gary", "Robbie", "Mark", "Howard", "Jason"]
+print (myList)
+
+myList.remove("Robbie") # Robbie Leaves in 1995
+print(myList)
+
+myList.append("Robbie") # Robbie rejoins in 2010
+print(myList)
+
+myList.remove("Robbie")
+myList.remove("Jason")
+
+myList.append("Ed")
+print (myList)
+
